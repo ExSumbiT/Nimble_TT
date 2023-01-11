6e93201bbfddf0544b7633e0a4fef7e99fe93717
Nikola <mts@3i.ua>
2023-01-11 12:51:26 +0200

    add refresh token and function for download file

--- app/dropbox_storage.py
+++ app/dropbox_storage.py
@@ -1,19 +1,28 @@
 import dropbox
 from dropbox.exceptions import AuthError
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 
 class Storage:
-    def __init__(self, access_token):
+    def __init__(self):
         try:
-            dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
+            self.dbx = dropbox.Dropbox(app_key=config['DROPBOX_APP_KEY'],
+                                       app_secret=config['DROPBOX_APP_SECRET'],
+                                       oauth2_refresh_token=config['DROPBOX_REFRESH_TOKEN'])
         except AuthError as e:
             print('Error connecting to Dropbox with access token: ' + str(e))
-        self.dbx = dropbox.Dropbox(access_token)
 
-    def upload_file(self, binary_file_stream, file_to):
-        """upload a file to Dropbox using API v2
-        """
+    def upload_file(self, binary_file_stream, path):
+        """upload a file to Dropbox"""
 
-        # with open(file_from, 'rb') as f:
-        # self.dbx.files_upload(f.read(), file_to)
-        self.dbx.files_upload(binary_file_stream.read(), file_to)
+        if self.dbx:
+            self.dbx.files_upload(binary_file_stream, path)
+
+    def download_file(self, path):
+        """download file from Dropbox"""
+
+        if self.dbx:
+            meta, resp = self.dbx.files_download(path)
+            return meta.name, resp.content
